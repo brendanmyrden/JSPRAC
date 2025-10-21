ffe2405e1c70a619e6e6cb6c64155d1d588f4593
MYRDEN <79550484+brendanmyrden@users.noreply.github.com>
2025-10-21 12:27:27 -0300

    Alerts, Math operations, Screenshot

---
+++ file.js
@@ -0,0 +1,28 @@
+// Alerts and Pop-Up's
+
+alert("Hello Brendan, here you are again");
+
+alert("Continue with the Udemy Popup");
+
+let variablesAreGreat = "amazing";
+
+if (variablesAreGreat === "amazing")
+  alert("I find this enjoyable now. I am a cyberpunk...");
+
+// Change the variable, break the pop-up
+
+// let variablesAreGreat = "non-chalant";
+
+// Run this
+
+// if (variablesAreGreat === 'amazing') alert ('I find this enjoyable now. I am a cyberpunk...')
+
+// Now the code no longer works
+
+40 + 20;
+
+40 - 20;
+
+4 * 5;
+
+4 / 5;
