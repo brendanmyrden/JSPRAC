ac3dc9741a7b6819833e39b38b1a228fe03e482e
MYRDEN <79550484+brendanmyrden@users.noreply.github.com>
2025-10-25 20:18:01 -0300

    Initial Comments Ability Added

--- file.js
+++ file.js
@@ -1,23 +1,41 @@
-// Alerts and Pop-Up's
+// = Alerts and Pop-Up's
+
 
 alert("Hello Brendan, here you are again");
 
-alert("Continue with the Udemy Popup");
+// H: alert("Continue with the Udemy Popup");
+
 
 let variablesAreGreat = "amazing";
 
 if (variablesAreGreat === "amazing")
+
   alert("I find this enjoyable now. I am a cyberpunk...");
 
-// Change the variable, break the pop-up
 
-// let variablesAreGreat = "non-chalant";
 
-// Run this
 
-// if (variablesAreGreat === 'amazing') alert ('I find this enjoyable now. I am a cyberpunk...')
+/* 
 
-// Now the code no longer works
+= Change the variable, break the pop-up
+
+| let variablesAreGreat = "nonchalant"
+
+
+= Run this
+
+| if (variablesAreGreat === 'amazing')
+
+|   alert ('I find this enjoyable now. I am a cyberpunk...')
+
+! Now the code no longer works
+
+*/ 
+
+
+
+
+// = Math Operators
 
 40 + 20;
 
