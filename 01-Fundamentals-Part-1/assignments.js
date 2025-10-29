b323f05d37d40525fb958ef7d1cfa56919c24f4c
MYRDEN <79550484+brendanmyrden@users.noreply.github.com>
2025-10-29 20:55:06 -0300

    F1:A2: Complete

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -7,9 +7,22 @@
 // = 1
 
 let country = "Canada"
-let continent = "North America"
-let population = "41,718,598"
+let continent = 'North America'
+let population = 41718598;
 
 console.log(country,',', continent,',', population);
+
+// SUM = Correct
+
+
+// = 2 Data Types
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof country);
+console.log(typeof population);
+console.log(typeof language);
 
 // SUM = Correct
