1caf39a676e9086591fb724121a04b20dcf64f57
MYRDEN <79550484+brendanmyrden@users.noreply.github.com>
2025-11-20 12:48:29 -0400

    F1: Expressions vs. Statements

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,22 @@
+// = Expression 
+
+// * Produces a value
+// | Examples
+
+3 + 4
+1991
+true && false && !false
+
+
+// = Statement // Declaration
+
+// * Large piece of code that is executed
+// * Doesn't produce a value itself
+
+// | Code is written in statements, consecutive actions
+
+// * EX
+
+if(23 > 10){
+    const str = '23 is bigger';
+}
