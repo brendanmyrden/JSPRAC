0f8f5c80a4c014edf15c458fd95bf84c77b742e6
MYRDEN <79550484+brendanmyrden@users.noreply.github.com>
2025-11-20 11:58:26 -0400

    F1: Switch Operator

--- notes.js
+++ notes.js
@@ -674,3 +674,58 @@ if(hasDriversLicense && hasGoodVision && !isTired) {
 
  //____________________________________________________________________________________________________________________
 
+// = SWITCH Statement // Alternative way to write an if/else statement
+
+const day = 'thursday';
+
+switch(day) {
+    
+    case 'monday':
+
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+
+    case 'tuesday': 
+        console.log('Prepare theory videos');
+        break;
+
+    case 'wednesday': 
+    case 'thursday': 
+        console.log('Write code examples');
+
+    case 'friday': 
+        console.log('Record videos');
+        break;
+
+    case 'saturday': 
+    case 'sunday':
+         console.log('Enjoy the weekend')
+         break;
+
+    default:
+        console.log('Not a valid day');
+
+}
+
+
+// } Alternative to Switch Statement
+
+if(day === 'monday'){
+
+    console.log(`Plan course structure`);
+    console.log(`Go to coding meetup`);
+
+} else if (day === 'tuesday'){
+
+    console.log(`Prepare the theory videos`);
+
+} else if (day === 'wednesday' || day === 'thursday'){
+
+    console.log('Write code examples');
+
+} else {
+    console.log('Not a valid day')
+}
+
+ //____________________________________________________________________________________________________________________
